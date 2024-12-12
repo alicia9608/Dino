a004f958f87d92c44b689bc63ea72128b1196659
alicia9608 <yu20071108@gmail.com>
2024-12-12 12:02:37 +0800

    v.2

---
+++ Dino/run.py
@@ -0,0 +1,103 @@
+import pygame
+
+pygame.init()
+#畫布大小
+screen = pygame.display.set_mode((1280,400)) 
+#載入圖片
+img_dino = pygame.image.load("dino.png")
+img_cactus = [pygame.image.load("DinoRun1.png"),pygame.image.load("DinoRun2.png")]
+img_dinorun=pygame.image.load("dino.png")
+#設定角色
+dino_rect=img_dino.get_rect()
+dino_rect.x=50
+dino_rect.y=300
+is_jumping=False
+jump=20
+nowjump=jump
+g=1 
+
+cactus_rect=img_cactus.get_rect()
+cactus_rect.x=2000
+cactus_rect.y=330
+speed=10
+#設定分數
+score=0
+highscore=0 #最高紀錄
+font=pygame.font.Font(None,36)
+
+clock = pygame.time.Clock()
+running = True
+gameover=False
+
+    
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    score+=1
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                is_jumping=True
+            if event.key == pygame.K_r:
+                score=0
+                cactus_rect.x=2000
+                gameover=False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+                is_jumping=True
+                if gameover:
+                    score=0
+                    cactus_rect.x=2000
+                    gameover=False
+
+    if not gameover:
+        
+        if is_jumping:
+            dino_rect.y-=nowjump
+            nowjump-=g
+            if dino_rect.y>300:
+                dino_rect.y=300
+                nowjump=jump
+                is_jumping=False
+
+        cactus_rect.x-=speed
+        if cactus_rect.x<0:
+            cactus_rect.x=1280
+        
+        if dino_rect.colliderect(cactus_rect):
+            if score>highscore:
+                highscore=score
+            
+            gameover=True 
+
+        # fill the screen with a color to wipe away anything from last frame
+        screen.fill((255,255,255))
+
+        score_show=font.render(f"Score:{score}",True,(0,0,0))
+        screen.blit(score_show,(10,10))
+
+        highscore_show=font.render(f"Hi Score:{highscore}",True,(0,0,0))
+        screen.blit(score_show,(10,30))
+
+        if gameover:
+            gameover_show=font.render(f"Game Over",True,(0,0,0))
+            screen.blit(gameover_show,(550,150))
+
+        # RENDER YOUR GAME HERE
+        screen.blit(img_dino,dino_rect)
+        screen.blit(img_cactus,cactus_rect)
+        # flip() the display to put your work on screen
+        pygame.display.flip()
+
+        clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
+
+
+
+
+
+
